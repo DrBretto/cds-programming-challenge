fc5a76767228375f3eec667d200613279d837a6c
Brett Westerlund <drbretto82@gmail.com>
2021-12-11 13:02:30 -0500

    added some basic realtime error captures for required form elements

--- app.js
+++ app.js
@@ -1,9 +1,59 @@
-const STORE = {};
+$(document).ready(function () {
 
-function main() {}
+  $("#firstName").on("input", function () {
+    let input = $(this);
+    let firstName = input.val();
+    if (firstName) {
+      input.removeClass("invalid").addClass("valid");
+    } else {
+      input.removeClass("valid").addClass("invalid");
+    }
+  });
 
-function eventListeners() {}
+  $("#lastName").on("input", function () {
+    let input = $(this);
+    let lastName = input.val();
+    if (lastName) {
+      input.removeClass("invalid").addClass("valid");
+    } else {
+      input.removeClass("valid").addClass("invalid");
+    }
+  });
 
-$(function () {});
+  $("#lastName").on("input", function () {
+    let input = $(this);
+    let name = input.val();
+    if (name) {
+      input.removeClass("invalid").addClass("valid");
+    } else {
+      input.removeClass("valid").addClass("invalid");
+    }
+  });
 
-$(main);
+
+  $("#email").on("input", function () {
+    let input = $(this);
+    let re =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let is_email = re.test(input.val());
+    if (is_email) {
+      input.removeClass("invalid").addClass("valid");
+    } else {
+      input.removeClass("valid").addClass("invalid");
+    }
+  });
+
+  $("#promo").on("input", function () {
+    let input = $(this);
+    let promo = input.val();
+
+    let
+
+    if (promo) {
+      input.removeClass("invalid").addClass("valid");
+    } else {
+      input.removeClass("valid").addClass("invalid");
+    }
+  });
+
+});
