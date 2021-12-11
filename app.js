9140449a0f2a0808919e3d7ad480a6c3490d9ec4
Brett Westerlund <drbretto82@gmail.com>
2021-12-11 16:14:14 -0500

    most jquery functionality complete

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-$(document).ready(function () {
+function validateAll() {
+  let numberValid = $(".valid").length;
+  console.log(numberValid);
+  if (numberValid === 8) {
+    $("#successBox").removeClass("unsuccessful").addClass("success");
+  } else {
+    $("#successBox").removeClass("success").addClass("unsuccessful");
+  }
+}
 
+$(document).ready(function () {
   $("#firstName").on("input", function () {
     let input = $(this);
     let firstName = input.val();
     if (firstName) {
-      input.removeClass("invalid").addClass("valid");
+      input
+        .next()
+        .removeClass("hidden")
+        .removeClass("invalid")
+        .addClass("valid");
+      validateAll();
     } else {
-      input.removeClass("valid").addClass("invalid");
+      input
+        .next()
+        .removeClass("hidden")
+        .removeClass("valid")
+        .addClass("invalid");
     }
   });
 
@@ -14,22 +32,39 @@ $(document).ready(function () {
     let input = $(this);
     let lastName = input.val();
     if (lastName) {
-      input.removeClass("invalid").addClass("valid");
+      input
+        .next()
+        .removeClass("hidden")
+        .removeClass("invalid")
+        .addClass("valid");
     } else {
-      input.removeClass("valid").addClass("invalid");
+      input
+        .next()
+        .removeClass("hidden")
+        .removeClass("valid")
+        .addClass("invalid");
     }
+    validateAll();
   });
 
-  $("#lastName").on("input", function () {
+  $("#phone").on("input", function () {
     let input = $(this);
-    let name = input.val();
-    if (name) {
-      input.removeClass("invalid").addClass("valid");
+    let phone = input.val();
+    if (!!phone.match(/^[0-9]+$/)) {
+      input
+        .next()
+        .removeClass("hidden")
+        .removeClass("invalid")
+        .addClass("valid");
     } else {
-      input.removeClass("valid").addClass("invalid");
+      input
+        .next()
+        .removeClass("hidden")
+        .removeClass("valid")
+        .addClass("invalid");
     }
+    validateAll();
   });
-
 
   $("#email").on("input", function () {
     let input = $(this);
@@ -37,23 +72,85 @@ $(document).ready(function () {
       /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     let is_email = re.test(input.val());
     if (is_email) {
-      input.removeClass("invalid").addClass("valid");
-    } else {
-      input.removeClass("valid").addClass("invalid");
-    }
-  });
+      input
+        .next()
+        .removeClass("hidden")
+        .removeClass("invalid")
+        .addClass("valid");
+      } else {
+        input
+        .next()
+        .removeClass("hidden")
+        .removeClass("valid")
+        .addClass("invalid");
+      }
+      validateAll();
+    });
 
   $("#promo").on("input", function () {
     let input = $(this);
     let promo = input.val();
-
-    let
-
-    if (promo) {
-      input.removeClass("invalid").addClass("valid");
+    if (!!promo.match(/^[0-9a-z]+$/) || promo.length === 0) {
+      input
+        .next()
+        .removeClass("hidden")
+        .removeClass("invalid")
+        .addClass("valid");
+      if (promo.length > 0) {
+        input
+          .nextAll("#howDidYouHear")
+          .removeClass("hidden")
+          .removeClass("invalid")
+          .addClass("valid");
+      }
     } else {
-      input.removeClass("valid").addClass("invalid");
+      input
+        .next()
+        .removeClass("hidden")
+        .removeClass("valid")
+        .addClass("invalid");
     }
+    validateAll();
   });
 
+  $("#howDidYouHear").on("input", function () {
+    let input = $(this);
+    let howDidYouHear = input.val();
+    if (howDidYouHear) {
+      input
+        .next()
+        .removeClass("hidden")
+        .removeClass("invalid")
+        .addClass("valid");
+    } else if (!input.prevAll("#promo").val()) {
+      input
+        .next()
+        .removeClass("hidden")
+        .removeClass("valid")
+        .addClass("invalid");
+    }
+    validateAll();
+  });
+
+  $("#terms").change(function () {
+    let input = $(this);
+    let terms = input.is(":checked");
+    console.log(terms);
+    if (terms) {
+      input
+        .next()
+        .next()
+        .removeClass("hidden")
+        .removeClass("invalid")
+        .addClass("valid");
+    } else {
+      input
+        .next()
+        .next()
+        .removeClass("hidden")
+        .removeClass("valid")
+        .addClass("invalid");
+    }
+    validateAll();
+  });
 });
